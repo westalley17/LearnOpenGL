19f2116f25054e693e95d3da11dab997a146e0d1
Wes Talley <wesatalley@gmail.com>
2025-04-29 10:15:40 -0500

    Even more jumpscare

--- src/Application.cpp
+++ src/Application.cpp
@@ -62,21 +62,21 @@ int main()
     Shader ourShader("src/shaders/vertexShader.glsl", "src/shaders/fragmentShader.glsl");
 
     /* Third(?), generate and apply our texture */
-    //stbi_set_flip_vertically_on_load(true);
+    stbi_set_flip_vertically_on_load(true);
     unsigned int texture1, texture2;
     glGenTextures(1, &texture1);
     glBindTexture(GL_TEXTURE_2D, texture1);
     // set the texture wrapping/filtering options (on the currently bound texture object)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load and generate the textures
     int width, height, nrChannels;
-    unsigned char* data = stbi_load("C:/Users/wesat/Desktop/LearnOpenGL/Dependencies/img/container.jpg", &width, &height, &nrChannels, 0);
+    unsigned char* data = stbi_load("C:/Users/wesat/Desktop/LearnOpenGL/Dependencies/img/travis_brummett.png", &width, &height, &nrChannels, 4);
     if (data)
     {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
     else
@@ -91,10 +91,10 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    data = stbi_load("C:/Users/wesat/Desktop/LearnOpenGL/Dependencies/img/awesomeface.png", &width, &height, &nrChannels, 0);
+    data = stbi_load("C:/Users/wesat/Desktop/LearnOpenGL/Dependencies/img/awesomeface.png", &width, &height, &nrChannels, 4);
     if (data)
     {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
     else
